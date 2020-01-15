9631c7cf17a352749003207543bd70ef506d687f
Treek <nbatre1@yahoo.com>
2020-01-15 13:17:50 -0600

    keep moving forward

--- app/app4.js
+++ app/app4.js
@@ -3,6 +3,26 @@ const fs = require('fs');
 
 
 fs.readFile('./db.json','utf-8',(err,data)=>{
+  
+    const d = JSON.parse(data);
+
+ console.log(d,'this is d')
+  return  console.log(d.Message,);
 
 
+})
+
+fs.readdir('./',function(err,data){
+    
+    console.log(data);
+})
+
+
+const temp ={
+    greeting:'Never Ever Give up'
+}
+
+
+fs.writeFile('db1.json',JSON.stringify(temp),function(err){
+    console.log('file created');
 })
